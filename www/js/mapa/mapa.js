6b07cb7a4d8017b76349a6d96db1792eb4eb8741
diegoalberione <diegoalberione@gmail.com>
2015-08-01 12:38:48 -0300

    modificaciones

--- www/js/mapa/mapa.js
+++ www/js/mapa/mapa.js
@@ -4,7 +4,7 @@ var overviewmapCtrol;
 var urlTile;
 var urlWMS;
 var mapFile;
-var urlGeoJson = 'http://190.12.101.74/ais/alpi/ws/entidades/listar/1';
+var urlGeoJson = 'http://190.12.101.74/ais/alpi/ws/entidades/listar/4';
 
 var posActual = [-6506141.183454158, -4110246.2464916063];
 var posInicial = [-6506141.183454158, -4110246.2464916063];
@@ -59,7 +59,7 @@ function inicializar(){
 	
 	view = new ol.View({
 		center: posInicial,
-		zoom: 13
+		zoom: 11
 	});
 	/**/
 	
@@ -70,26 +70,55 @@ function inicializar(){
 	});
 	
 	map.on('singleclick', function(evt) {
-		demoPunto();
+		/* verifica si hay entidades en layers tipo vector GeoJSON */
+		map.forEachFeatureAtPixel(evt.pixel, function(feature, layer) {
+			if ((layer.get('type') != 'base') && (layer.get('nombre') != 'vectorLyBusqueda') && (layer.get('nombre') != 'vectorLyDibujo')){
+				$('#infoEntidadTitulo').html(feature.get('nombre'));
+				$('#infoEntidadCopete').html(feature.get('rubro')+'<br>'+feature.get('calle')+' '+feature.get('altura'));
+				var htmlDetalle = "";
+				htmlDetalle += '<b>Tel.: </b>'+feature.get('telefono')+'<br>';
+				htmlDetalle += '<b>e-mail: </b>'+feature.get('email')+'<br>';
+				htmlDetalle += '<b>Web: </b>'+feature.get('web')+'<br>';
+				htmlDetalle += feature.get('descripcion')+'<br>';
+				$('#divInfoEntidadDetalle').html(htmlDetalle);
+				
+				var ms = feature.get('Multimedias');
+				$('#infoEntidadImg').attr('src','./img/sinimagen.png');
+				$('#infoEntidadImgA').attr('href',"javascript:imageAlert('./img/sinimagen.png')");
+				if (typeof ms[0] != 'undefined'){
+					var m = ms[0];
+					var urlimg = feature.get('MultimediasUrl')+m.Multimedia.codigo;
+					$('#infoEntidadImg').attr('src',urlimg);	
+					$('#infoEntidadImgA').attr('href',"javascript:imageAlert('"+urlimg+"')");		
+				}
+				$("#divInfoEntidad").show(500);
+			}
+		});
 	});
-	
+
 	var vectorLayer = new ol.layer.Vector({
 		id: 1,
+		type: 'overlay',
+		nombre: 'lugaresoficiales',
+		titulo: 'Lugares oficiales', 
+		mapFile: '', 
+		icono: 'http://190.12.101.74/ais/alpi/files/icons_layers/lugaresoficiales.png',
+		datasource: 1,
+		visible: true,
 		source: new ol.source.GeoJSON({
 			projection: 'EPSG:3857',
 			url: urlGeoJson
 		}),
 		style: function(feature, resolution) {
-			//console.log(feature.get('icono'));
 			var iconStyle = null;
-			if (feature.get('icono') != null){
+			if (true){ //(feature.get('icono') != null){
 				iconStyle = [new ol.style.Style({
 					image: new ol.style.Icon( ({
 						anchor: [16, 32],
 						anchorXUnits: 'pixels',
 						anchorYUnits: 'pixels',
 						opacity: 0.75,
-						src: './iconos/lugaresoficiales.png'
+						src: 'http://190.12.101.74/ais/alpi/files/icons_layers/lugaresoficiales.png'
 					}))
 				})];
 			}
