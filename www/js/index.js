5f147dca0464243258b0866b1d80dd3f84019e37
gabrielalberione <gabrielalberione@gmail.com>
2015-08-27 08:53:21 -0300

    v1.1

--- www/js/index.js
+++ www/js/index.js
@@ -18,7 +18,8 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
 		navigator.splashscreen.hide();
-		navigator.geolocation.getCurrentPosition(onSuccess, onError);     
+		navigator.geolocation.getCurrentPosition(onSuccess, onError);  
+		checkConnection();   
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -60,4 +61,20 @@ function onSuccess(position) {
 
 function onError(error) { 
   // your callback here
+}
+
+function checkConnection() {
+	var networkState = navigator.connection.type;
+
+	var states = {};
+	states[Connection.UNKNOWN]  = 'Unknown connection';
+	states[Connection.ETHERNET] = 'Ethernet connection';
+	states[Connection.WIFI]     = 'WiFi connection';
+	states[Connection.CELL_2G]  = 'Cell 2G connection';
+	states[Connection.CELL_3G]  = 'Cell 3G connection';
+	states[Connection.CELL_4G]  = 'Cell 4G connection';
+	states[Connection.CELL]     = 'Cell generic connection';
+	states[Connection.NONE]     = 'No network connection';
+
+	alert('Connection type: ' + states[networkState]);
 }
